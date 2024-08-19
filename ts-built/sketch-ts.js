c7b7ed79fbee3d30fbf78b564e73b1bad930437d
kemalmao19 <sebelumtidur14@gmail.com>
2024-08-19 19:23:51 +0700

    add gitignore

--- ts-built/sketch-ts.js
+++ ts-built/sketch-ts.js
@@ -3,14 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const canvasSketch = require('canvas-sketch');
 const canvas_sketch_1 = __importDefault(require("canvas-sketch"));
 const settings = {
-    dimensions: [2048, 2048]
+    dimensions: [2048, 2048],
 };
 const sketch = () => {
     return ({ context, width, height }) => {
-        context.fillStyle = 'white';
+        context.fillStyle = "white";
         context.fillRect(0, 0, width, height);
     };
 };
