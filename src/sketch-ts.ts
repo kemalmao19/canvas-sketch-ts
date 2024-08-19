7d73d8ff86adf4189586c8525ef8328d9c98820f
kemalmao19 <sebelumtidur14@gmail.com>
2024-08-19 12:01:59 +0700

    remove comment

--- src/sketch-ts.ts
+++ src/sketch-ts.ts
@@ -1,19 +1,18 @@
-// const canvasSketch = require('canvas-sketch');
-import canvasSketch from 'canvas-sketch';
+import canvasSketch from "canvas-sketch";
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048],
 };
 
 type Canvas = {
-  context: CanvasRenderingContext2D,
-  width: number,
-  height: number,
+  context: CanvasRenderingContext2D;
+  width: number;
+  height: number;
 };
 
 const sketch: () => void = () => {
   return ({ context, width, height }: Canvas) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
   };
 };
